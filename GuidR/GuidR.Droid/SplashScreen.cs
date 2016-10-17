c3d2d2434fba12ccb16685a9a29a556e0cc8cc27
Kasper Røgen <kasperhangard@hotmail.com>
2016-10-17 14:45:40 +0200

    Initial Commit

---
+++ GuidR/GuidR.Droid/SplashScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GuidR.Droid {
+    [Activity(Label ="My Splash Screen", MainLauncher = true, NoHistory = true, Theme ="@style/Theme.splash", Icon ="@drawable/logo")]
+    public class SplashScreen: Activity {
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+            Thread.Sleep(4000);
+            StartActivity(typeof(MainActivity));
+        }
+
+    }
+
+
+}
---
+++ GuidR/GuidR/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuidR
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
