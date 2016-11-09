421538cf41bc27a44e9daae8894b6db53c7a59c4
Mads Alberg Christensen <madsahtxq12@gmail.com>
2016-11-09 13:21:16 +0100

    News interface temporary fix - Database needed for further process.

--- GuidR/GuidR.Droid/NewsActivity.cs
+++ GuidR/GuidR.Droid/NewsActivity.cs
@@ -5,14 +5,17 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Support.V4;
+using Android.Support.V7;
+using Android.Support.V7.CardView;
 using Android.Support.V7.Widget;
 
 namespace GuidR.Droid
 {
     [Activity(Label = "Aalborg Zoo", Theme = "@style/NoTitle.splash")]
-    public class NewsActivity : MainActivity
+    public class NewsActivity : Activity
     {
-        
+
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -21,11 +24,37 @@ namespace GuidR.Droid
 
             SetContentView(Resource.Layout.News);
 
-            
-
+            AddCardToNews(5);
 
         }
 
+        public void AddCardToNews(int i) {
+            for (int n = 0; n < i; n++)
+            {
+                LinearLayout cardContainer = FindViewById<LinearLayout>(Resource.Id.cardContainer);
+
+                CardView card = new CardView(this);
+                card.Id = 987654114;
+                card.SetMinimumHeight(200);
+                card.SetMinimumWidth(200);
+                card.SetContentPadding(2, 1, 1, 2);
+                cardContainer.AddView(card);
+
+                ImageView img = new ImageView(this);
+                img.SetMinimumHeight(100);
+                img.SetMinimumWidth(100);
+                img.SetBackgroundColor(Android.Graphics.Color.Blue);
+                card.AddView(img);
+
+                LinearLayout space = new LinearLayout(this);
+                space.SetMinimumHeight(20);
+                space.SetMinimumWidth(20);
+                cardContainer.AddView(space);
+
+            }
+
+
+        }
 
     }
 }
